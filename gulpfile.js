5703b58f68e50e633344c7c48ce9bd758d64eea1
Marcus Vinicius <marcusvinicius30798@gmail.com>
2015-01-21 10:59:36 -0300

    Gulp ajuste
    Gulp ajustado para que browser-sync funcione com o jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,69 @@
-var gulp 		= require('gulp');
-var pumbler 	= require('gulp-plumber');
-var browserSync = require('browser-sync');
-var reload 		= browserSync.reload;
-var less 		= require('gulp-less');
-var runSequence = require('run-sequence');
-var imagemin    = require('gulp-imagemin');
+var gulp 		 = require('gulp');
+var pumbler 	 = require('gulp-plumber');
+var browserSync  = require('browser-sync');
+var reload 		 = browserSync.reload;
+var less 		 = require('gulp-less');
+var runSequence  = require('run-sequence');
+var imagemin     = require('gulp-imagemin');
+var cp           = require('child_process');
+var uglify 		 = require('gulp-uglify');
+var concat 		 = require('gulp-concat');
 
 
-gulp.task('browser-sync',function () {
-	browserSync({
-		notify:true,
-		port: 3000,
-		proxy: "localhost:4000"
-	});	
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    //Linux/Mac
+    //return cp.spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', done);
+	//WINDOWS
+    return cp.exec('jekyll', ['build'], {stdio: 'inherit'}).on('close', done);
 });
 
 
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+
+gulp.task('browser-sync', ['less', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+// gulp.task('browser-sync',function () {
+// 	browserSync({
+// 		notify:true,
+// 		port: 3000,
+// 		proxy: "localhost:4000"
+// 	});	
+// });
+
+//Less Task
 gulp.task('less',function(){
-	gulp.src('src/less/main.less')
+	gulp.src('src/less/main.less')	
 	.pipe(less({compress: true}))
-	.pipe(gulp.dest('./assets/css'))
-	.pipe(reload({stream:true}))
-
+	.pipe(gulp.dest('_site/assets/css'))
+	.pipe(browserSync.reload({stream:true}))
+	.pipe(gulp.dest('assets/css'))
 });
 
 
+//Js Task
+gulp.task('js', function (){
+	return gulp.src('src/js/**/*')
+	.pipe(pumbler())
+	.pipe(concat('main.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/js'))
+});
+
+// Imagemin Task
 gulp.task('imagemin', function(){
 	return gulp.src('src/img/**/*')
 	.pipe(pumbler())
@@ -32,9 +71,11 @@ gulp.task('imagemin', function(){
 	.pipe(gulp.dest('assets/img'))
 });
 
-gulp.task('serve', function (){
-	runSequence('less','browser-sync');	
-	gulp.watch(['_layouts/*.html','_includes/*.html','./*.html'], reload);	
-	gulp.watch(['./src/less/*.less'], ['less', reload]);
-	gulp.watch('src/img/**/*.{jpg,png,gif}',['imagemin', reload]);
+gulp.task('watch', function (){
+	gulp.watch('src/less/**/*.less', ['less']);
+	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/img/**/*.{jpg, png, gif}', ['imagemin']);
+	gulp.watch(['*.html','index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
+
+gulp.task('default', ['js', 'less', 'imagemin', 'browser-sync', 'watch']);
